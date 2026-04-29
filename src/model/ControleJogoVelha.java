995f33cd99ea2eded827013c6a7f64faf8fbfcc6
Giovanni Ferreira <giovanniferreira@mx2.unisc.br>
2026-04-28 21:33:32 -0300

    Jogo pronto
    Jogo totalmente funcional

--- src/model/ControleJogoVelha.java
+++ src/model/ControleJogoVelha.java
@@ -8,22 +8,79 @@ public class ControleJogoVelha extends ControleJogo {
 
     @Override
     public void iniciarPartida() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        tabuleiro = "123456789".toCharArray();
+        tempoInicio = System.currentTimeMillis();
+        
     }
-
+    
     @Override
     public void adicionarMovimento(Character letra, int pos) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        tabuleiro[pos] = letra;
     }
 
     @Override
     public long getDuracaoPartida() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        long agora = System.currentTimeMillis();
+        return agora - tempoInicio;
     }
 
     @Override
     public char avaliarPartida() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        int [][] combinacoes =  {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            
+            {0,4,8},
+            {2,4,6}
+        };
+        
+        for (int[] c : combinacoes) {
+
+        if (tabuleiro[c[0]] != ' ' &&
+            tabuleiro[c[0]] == tabuleiro[c[1]] &&
+            tabuleiro[c[1]] == tabuleiro[c[2]]) {
+
+            return tabuleiro[c[0]];
+        }
+        }
+    boolean cheio = true;
+
+        for (char c : tabuleiro){
+        if (c == ' '){
+            cheio = false;
+            break;
+        }
+        }
+        
+        if (cheio){
+            return 'E';
+        }
+        
+        return 0;
     
+    }
+        
+    public String getStatusPartida(){
+        String ret ="";
+        switch (avaliarPartida()) {
+            case 'X':
+                ret = "Jogador X Venceu";
+                break;
+            case 'O':
+                ret = "Jogador O Venceu";
+                break;
+            case 'E':
+                ret = "Partida Empatada";
+                break;
+            default:
+                ret = "Partida em Andamento";
+        }
+        ret += String.format(" (%.2f segundos)", getDuracaoPartida()/1000.0);
+        return ret;
+    }
 }
--- src/view/FrmJogoVelha.java
+++ src/view/FrmJogoVelha.java
@@ -1,7 +1,7 @@
-
 package view;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.HashSet;
 import javax.swing.*;
 
@@ -16,37 +16,79 @@ public class FrmJogoVelha extends JFrame {
     private JRadioButton rbO = new JRadioButton("Jogador O", true);
     private JButton[] botoes = new JButton[9]; 
     private JTextArea txt = new JTextArea(5,10);
-   
+    private ButtonGroup grupo = new ButtonGroup();
+    private model.ControleJogoVelha regras = new model.ControleJogoVelha();
     
     public FrmJogoVelha(){
         super("Jogo da Velha");
-        //-----Painel superior------
+        //----Painel superior-----
         JPanel pnlCima = new JPanel();
         pnlCima.add(btnNovoJogo);
-        pnlCima.add(rbX);
-        pnlCima.add(rbO);
-        this.getContentPane().add(pnlCima, BorderLayout.NORTH);
-        //------------
+        btnNovoJogo.addActionListener(new Ouvinte());//vincular o botao ao listener
         
-        //-----Painel Meio-------
+        
+        pnlCima.add(rbX).setEnabled(false);
+        pnlCima.add(rbO).setEnabled(false);
+        grupo.add(rbX);
+        grupo.add(rbO);
+        this.getContentPane().add(pnlCima, BorderLayout.NORTH);
+        //-----------------------
+        
+        //------Painel Meio------
         JPanel pnlBotoes = new JPanel();
         pnlBotoes.setLayout(new GridLayout(3,3));
         for(int i=0;i<9; i++){
             botoes[i] = new JButton(Integer.toString(i+1));
+            botoes[i].addActionListener(new Ouvinte());
             pnlBotoes.add(botoes[i]);
         }
         this.getContentPane().add(pnlBotoes, BorderLayout.CENTER);
-        //------------
+        //-----------------------
         
         
         //------Parte Baixo------
         
         this.getContentPane().add(txt, BorderLayout.SOUTH);
         
-        //------------
+        //-----------------------
         this.setBounds(100,100,450, 400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         
     }
+    //classe interna para eventos
+    private class Ouvinte implements ActionListener{
+    
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource() == btnNovoJogo){
+                for(int i=0; i<9; i++){
+            botoes[i].setEnabled(true);
+            botoes[i].setText(Integer.toString(i + 1));
+            }
+            regras.iniciarPartida();
+            txt.setText(regras.getStatusPartida());
+        }else{
+            Character letra;
+            if(rbX.isSelected()){
+                letra = 'X';
+                rbO.setSelected(true);
+            }
+            else{
+                letra = 'O';
+            rbX.setSelected(true);
+            }
+            int pos = Integer.parseInt(e.getActionCommand());
+             regras.adicionarMovimento(letra, pos-1);
+             
+             botoes[pos-1].setEnabled(false);
+             botoes[pos-1].setText(letra.toString());
+             
+             txt.setText(regras.getStatusPartida());
+            }
+                    
+                    }
+    
+    
+}
 }
